3a5304a104949a1366c5d2aae3ed4c35e562db35
nester1van <nester1van@yandex.ru>
2020-05-22 17:28:01 +0400

    add home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import {BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Home from './projects/Home/Home.js';
 import RandomPictDog from '../src/projects/randomPictDog/';
 import DogPictsByBreed from './projects/dogPictsByBreed';
 import GitHubClient from './projects/gitHubClient';
+
 
 function App() {
   return (
@@ -17,19 +19,19 @@ function App() {
                 <Link className="link" to="/">Home</Link>
               </li>
               <li>
-                <Link className="link" to="/randompictdog">RandomPictDog</Link>
+                <Link className="link" to="/randompictdog">Random dog picture</Link>
               </li>
               <li>
-                <Link className="link" to="/dogpictsbybreed">DogPictsByBreed</Link>
+                <Link className="link" to="/dogpictsbybreed">Dog pictures by breed</Link>
               </li>
               <li>
-                <Link className="link" to="/githubclient">GitHubClient</Link> 
+                <Link className="link" to="/githubclient">GitHub Client</Link> 
               </li>
             </ul>
           </nav>
           <main>
           <Switch>
-              <Route exact path="/">Home</Route>
+              <Route exact path="/"><Home/></Route>
               <Route path="/randompictdog"><RandomPictDog/></Route>
               <Route path="/dogpictsbybreed"><DogPictsByBreed/></Route>
               <Route path="/githubclient"><GitHubClient/></Route>  
---
+++ src/projects/Home/Home.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <>
+            <h1>Проекты на React</h1>
+            <h2>Random dog picture</h2>
+            <p>
+                Выводится случайная фотография собаки.
+                <br/>
+                Отображается статус выполнения запроса к серверу посредстом 
+                <a 
+                    href="https://dog.ceo/dog-api/" 
+                    className="linkApi"
+                    target="_blank" 
+                    rel="noopener noreferrer">
+                        DOG REST API
+                </a>
+            </p>
+            <h2>Get picture by breed</h2>
+            <p>
+                Выводится заданное количество случайных фотографий собак выбранной породы.
+                <br/>
+                Используется 
+                <a 
+                    href="https://dog.ceo/dog-api/" 
+                    className="linkApi"
+                    target="_blank" 
+                    rel="noopener noreferrer">
+                        DOG REST API
+                </a>
+            </p>
+            <h2>Github client</h2>
+            <p>
+                Реализован интерфейс доступа к разделам GitHub:
+                User information, Followers, Followings, Repositories, Zen.
+                <br/>
+                Используeтся 
+                <a 
+                    href="https://developer.github.com/v3/" 
+                    className="linkApi"
+                    target="_blank" 
+                    rel="noopener noreferrer">
+                        GitHub REST API v3
+                </a>                
+            </p>
+        </>
+    )
+}
+
+export default Home;
